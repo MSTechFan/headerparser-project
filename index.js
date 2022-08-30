b1b0ded6fbce9332c462b11f594db9707fc3cd1a
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-08-30 08:18:15 -0500

    completing headerparser express

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
+
+app.enable('trust proxy')
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -19,10 +21,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+
+// sofware: req.headers['user-agent'] language: req.headers['accept-language']
+
+
+
+app.get('/api/whoami', (req, res) => {
+  console.log(req.header('x-forwarded-for'))
+  res.json({"ipaddress": req.ip, "language": req.headers['accept-language'], "software":req.headers['user-agent']})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
